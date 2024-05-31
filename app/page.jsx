b7c247362dd80f79f86450b8e3c88f6994843e70
cogsincogs <jamiewthomas13@gmail.com>
2024-05-31 19:05:36 +0100

    Set up basic test page

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,19 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [test, setTest] = useState("")
+    const [video, setVideo] = useState("")
 
     useEffect(() => {
-        fetch(process.env.backendUrl + "/api/test")
+        fetch(process.env.backendUrl + "/api/video/1")
             .then(res => res.json())
-            .then(data => setTest(data.message))
+            .then(data => setVideo(data[0]))
     })
 
     return (
         <div>
             <h1 className="text-4xl font-bold">See below for test</h1>
-            <p>{test}</p>
+            <p>{video.title}</p>
+            <video src={video.path} controls></video>
         </div>
     );
 }
