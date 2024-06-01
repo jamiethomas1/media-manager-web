58e0e2ba315c1bd0964c4e06f79d45ecb83deb0c
cogsincogs <jamiewthomas13@gmail.com>
2024-06-02 00:23:42 +0100

    Add studio names

--- app/_components/VideoCard.jsx
+++ app/_components/VideoCard.jsx
@@ -1,11 +1,23 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
 export default function VideoCard({ video }) {
+    const [studio, setStudio] = useState("")
+
+    useEffect(() => {
+        fetch(process.env.backendUrl + "/api/studio/" + video.studio_id)
+            .then(res => res.json())
+            .then(data => setStudio(data))
+    }, [video.studio_id])
+
     return (
         <div className="border border-red-900 grid-item p-4 break-words">
             <p className="text-sm">{video.title}</p>
-            <p>{video.studio_id}</p>
+            <p>{studio.name}</p>
             <img
-                src={process.env.backendUrl + '/' + video.thumbnails[2]}
-                alt={process.env.backendUrl + '/' + video.thumbnails[2]}
+                src={process.env.backendUrl + '/' + video.thumbnails[0]}
+                alt={video.title}
                 width={320}
                 height={180}
             />
