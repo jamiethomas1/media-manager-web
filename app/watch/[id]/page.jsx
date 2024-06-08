a133aec0cd883202d08548079b86ef9195754c00
cogsincogs <jamiewthomas13@gmail.com>
2024-06-08 11:12:39 +0100

    Switch to server component and add release date to video page

--- app/watch/[id]/page.jsx
+++ app/watch/[id]/page.jsx
@@ -1,37 +1,60 @@
-'use client'
+export default async function Video({ params }) {
+    let video = await getVideo(params.id)
+    let studio = await getStudio(video.studio_id)
+    let cast = await getCast(video.id)
 
-import { useState, useEffect } from "react"
-
-export default function Video({ params }) {
-    const [video, setVideo] = useState({})
-    const [studio, setStudio] = useState({})
-    const [cast, setCast] = useState([])
-
-    useEffect(() => {
-        fetch("http://localhost:8000/api/video/" + params.id)
-            .then(res => res.json())
-            .then(data => setVideo(data))
-    }, [params.id])
-
-    useEffect(() => {
-        fetch("http://localhost:8000/api/studio/" + video.studio_id)
-            .then(res => res.json())
-            .then(data => setStudio(data))
-    }, [video.studio_id])
-
-    useEffect(() => {
-        fetch("http://localhost:8000/api/cast/" + video.id)
-            .then(res => res.json())
-            .then(data => setCast(data))
-    }, [video.id])
+    const dateOptions = {
+        day: "numeric",
+        month: "short",
+        year: "numeric"
+    }
+    const release_date = new Date(Date.parse(video.release_date)).toLocaleDateString("en-GB", dateOptions)
 
     return (
         <div>
             <p>{video.title}</p>
             <video src={"http://localhost:8000/" + video.path} controls />
-            <p>{video.date}</p>
             <p>{studio.name}</p>
+            <p>{release_date}</p>
             {cast.map(actress => <p key={actress.id}>{actress.name}</p>)}
         </div>
     )
+}
+
+async function getVideo(id) {
+    const res = await fetch(process.env.backendUrl + "/api/video/" + id)
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    
+    const data = await res.json()
+
+    return data
+}
+
+async function getStudio(id) {
+    const res = await fetch(process.env.backendUrl + "/api/studio/" + id)
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    
+    const data = await res.json()
+
+    console.log(data)
+
+    return data
+}
+
+async function getCast(id) {
+    const res = await fetch(process.env.backendUrl + "/api/cast/" + id)
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    
+    const data = await res.json()
+
+    return data
 }
