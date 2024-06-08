50b682c574ca9923a052c1ac0646567e55e05c7f
cogsincogs <jamiewthomas13@gmail.com>
2024-06-08 11:13:02 +0100

    Add release date and duration to all videos page

--- app/_components/VideoCard.jsx
+++ app/_components/VideoCard.jsx
@@ -1,12 +1,25 @@
 import HoverThumbnail from "./HoverThumbnail";
 
 export default async function VideoCard({ video, studio, cast }) {
+    const dateOptions = {
+        day: "numeric",
+        month: "short",
+        year: "numeric"
+    }
+    const release_date = new Date(Date.parse(video.release_date)).toLocaleDateString("en-GB", dateOptions)
+
+    let date = new Date(0);
+    date.setSeconds(video.length);
+    let duration = date.toISOString().substring(11, 19).replace(/^[0:]+/, "");
+
     return (
         <div className="rounded grid-item p-4 break-words bg-zinc-600">
             <a href={"/watch/" + video.id}>
                 <p className="text-base">{video.title}</p>
                 {cast.map(actress => <p key={actress.id}>{actress.name}</p>)}
                 <p className="text-sm">{studio.name}</p>
+                <p className="text-sm">{release_date}</p>
+                <p className="text-sm">{duration}</p>
                 <HoverThumbnail thumbnails={video.thumbnails} />
             </a>
         </div>
